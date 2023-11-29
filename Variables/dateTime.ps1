57ed507298d44c004bc9f47f0c7ad2e3417829de
Dante Falls <dante.falls@yahoo.com>
2023-11-28 18:28:25 -0800

    Create dateTime.ps1

---
+++ Variables/dateTime.ps1
@@ -0,0 +1,7 @@
+# The following PowerShell variable retrieves the current date and time.
+
+#Place the date and time in a variable
+$Today = (Get-Date).DateTime
+
+#Call the variable to display the date and time.
+$Today
