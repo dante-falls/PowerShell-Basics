1e0291c61f53ae981cdc0eb91b6f7466abe1a174
Dante Falls <dante.falls@yahoo.com>
2023-11-28 23:36:47 -0800

    Update startNotepad.ps1

--- Cmdlets/startNotepad.ps1
+++ Cmdlets/startNotepad.ps1
@@ -1,3 +1,3 @@
-#startNotepad.ps1 is a PowerShell script that starts the Notepad by using the "Start-Process" cmdlet.
+#startNotepad.ps1 is a PowerShell script that starts the Notepad by using the "Start-Process" cmdlet. You can right click this program and "Run With PowerShell", or you can run this program directly from the PowerShell command line.
 
 Start-Process Notepad.exe
